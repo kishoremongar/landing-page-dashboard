68467030ea46ee2a78d1889de18f0990b4ec6c03
Kishore Mongar <84139548+fushigina-Shinobi@users.noreply.github.com>
2024-06-07 01:19:32 +0530

    refactor: added password view toggle

--- src/app/auth/login/_components/loginMainLayout.jsx
+++ src/app/auth/login/_components/loginMainLayout.jsx
@@ -1,6 +1,8 @@
 'use client';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import OpenEyeIcon from '@@assets/icons/eyeOpen.svg';
+import CloseEyeIcon from '@@assets/icons/eyeClose.svg';
 import { setInLocalStorage } from '@/utils/localStorage';
 import { generateRandomToken } from '@/utils/generateFakeToken';
 import PrimaryButton from '@/components/common/primaryButton';
@@ -12,6 +14,7 @@ const LoginMainLayout = () => {
     success: false,
   });
   const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -24,21 +27,33 @@ const LoginMainLayout = () => {
     }
     setFormStatus({ loading: true, error: false, success: false });
 
-    if (email === 'demo@user.com' && password === 'Abcd.1234') {
-      const accessToken = generateRandomToken(32);
-      const refreshToken = generateRandomToken(64);
-      setInLocalStorage('user', {
-        email,
-        password,
-        isAuthenticated: true,
-        accessToken,
-        refreshToken,
-      });
-      setFormStatus({ loading: false, error: false, success: true });
-      router.push('/dashboard');
-    } else {
+    try {
+      if (email === 'demo@user.com' && password === 'Abcd.1234') {
+        const accessToken = generateRandomToken(32);
+        const refreshToken = generateRandomToken(64);
+        setInLocalStorage('user', {
+          email,
+          password,
+          isAuthenticated: true,
+          accessToken,
+          refreshToken,
+        });
+        setFormStatus({ loading: false, error: false, success: true });
+        router.push('/dashboard');
+      } else {
+        setFormStatus({ loading: false, error: true, success: false });
+      }
+    } catch (error) {
       setFormStatus({ loading: false, error: true, success: false });
+    } finally {
+      setTimeout(() => {
+        setFormStatus({ loading: false, error: false, success: false });
+      }, 3000);
     }
+  };
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
   };
 
   return (
@@ -46,7 +61,7 @@ const LoginMainLayout = () => {
       <div className='w-full flex flex-col gap-y-4 bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto'>
         {formStatus.error && (
           <span className='flex justify-center text-sm text-red-400'>
-            Invalid credentials
+            Invalid credentials!!!
           </span>
         )}
         <span className='block w-full text-center text-xl uppercase font-bold'>
@@ -74,14 +89,29 @@ const LoginMainLayout = () => {
             <label htmlFor='password' className='block text-xs mb-1'>
               Password
             </label>
-            <input
-              className='w-full border rounded p-2 outline-none focus:shadow-outline'
-              type='password'
-              name='password'
-              id='password'
-              placeholder='Password'
-              required
-            />
+            <div className='relative'>
+              <input
+                className='w-full border rounded p-2 outline-none focus:shadow-outline pr-10'
+                type={showPassword ? 'text' : 'password'}
+                name='password'
+                id='password'
+                placeholder='Password'
+                required
+              />
+              <div className='absolute inset-y-0 right-0 flex items-center pr-3'>
+                {showPassword ? (
+                  <OpenEyeIcon
+                    className='h-5 w-5 text-gray-400 cursor-pointer'
+                    onClick={handleTogglePassword}
+                  />
+                ) : (
+                  <CloseEyeIcon
+                    className='h-5 w-5 text-gray-400 cursor-pointer'
+                    onClick={handleTogglePassword}
+                  />
+                )}
+              </div>
+            </div>
           </div>
           <PrimaryButton type='submit' filled>
             Login
